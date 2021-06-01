8d1d1702822c45b0b309bf71ebffcc40dec1a88b
Emre <eemmrree0@outlook.com>
2021-06-01 15:33:11 +0300

    N-Katmali-Mimari
    Entity Framework ile Database in oluşturulmasi

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        private GenericRepos<Category> repo = new GenericRepos<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repo.List();
+        }
+
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" ||
+                p.CategoryName.Length >= 51)
+            {
+                //hata yeri
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/GenericRepos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepos<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+        readonly DbSet<T> _object;
+
+        public GenericRepos()
+        {
+            _object = c.Set<T>();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+
+        }
+
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+    }
+}
